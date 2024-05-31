f896941c5119bb2a117505978d7c999499e51a16
Wiseman3AM <amukelani210@gmail.com>
2024-05-31 12:18:10 +0200

    main

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from './data.json';
+import { defineConfig } from 'vite'
+
+export default {
+  plugins: [handlebars({
+    context : data   
+  })],
+};
+
+
+
+
